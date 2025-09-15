52ed2dba59ea3af715bc8eeba3fd1a76c7320217
Nirav Mangukiya <niravmangukiya81@gmail.com>
2025-09-15 16:44:12 +0000

    review card navigation arrow show out side the screen please fix this is

--- src/components/layout/CustomerReviews.tsx
+++ src/components/layout/CustomerReviews.tsx
@@ -61,41 +61,43 @@ export function CustomerReviews() {
         <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-20 h-1 bg-primary"></div>
       </h2>
       
-      <Carousel
-        opts={{
-          align: "start",
-          loop: true,
-        }}
-        className="w-full max-w-6xl mx-auto mt-12"
-      >
-        <CarouselContent className="-ml-4">
-          {reviews.map((review, index) => (
-            <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">
-                <Card className="h-full">
-                  <CardContent className="flex flex-col justify-between h-full p-6 text-left">
-                    <div className="flex-grow">
-                        {renderStars(review.rating)}
-                        <p className="text-muted-foreground text-sm mt-4">&quot;{review.comment}&quot;</p>
-                    </div>
-                    <div className="flex items-center gap-3 mt-6 pt-6 border-t">
-                        <Avatar className="w-12 h-12">
-                          <AvatarImage src={review.avatar} alt={review.name} />
-                          <AvatarFallback>{review.name.charAt(0)}</AvatarFallback>
-                        </Avatar>
-                        <div>
-                            <p className="font-semibold">{review.name}</p>
-                        </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="absolute left-[-2rem] top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
-        <CarouselNext className="absolute right-[-2rem] top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
-      </Carousel>
+      <div className="relative w-full max-w-6xl mx-auto mt-12">
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="w-full"
+        >
+          <CarouselContent className="-ml-4">
+            {reviews.map((review, index) => (
+              <CarouselItem key={index} className="pl-4 md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card className="h-full">
+                    <CardContent className="flex flex-col justify-between h-full p-6 text-left">
+                      <div className="flex-grow">
+                          {renderStars(review.rating)}
+                          <p className="text-muted-foreground text-sm mt-4">&quot;{review.comment}&quot;</p>
+                      </div>
+                      <div className="flex items-center gap-3 mt-6 pt-6 border-t">
+                          <Avatar className="w-12 h-12">
+                            <AvatarImage src={review.avatar} alt={review.name} />
+                            <AvatarFallback>{review.name.charAt(0)}</AvatarFallback>
+                          </Avatar>
+                          <div>
+                              <p className="font-semibold">{review.name}</p>
+                          </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-[-3.5rem] top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
+          <CarouselNext className="absolute right-[-3.5rem] top-1/2 -translate-y-1/2 z-10 hidden md:flex" />
+        </Carousel>
+      </div>
     </div>
   );
 }
