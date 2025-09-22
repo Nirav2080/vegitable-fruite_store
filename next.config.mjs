ca61bc8217692889087fbab61d6cbbd8c63fd8a2
Nirav Mangukiya <niravmangukiya81@gmail.com>
2025-09-22 05:43:32 +0000

    api show this error
    0:["$@1",["development",null]]
    1:E{"digest":"39342

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.icons8.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-      },
-    ],
+  serverActions: {
+    bodySizeLimit: '4mb',
   },
 };
 
